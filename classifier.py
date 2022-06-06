fbe55ddafebe9c752d52c84cadc79aaf84cd9360
swiatowiec <katarzynaswiatowiec@gmail.com>
2022-06-06 21:54:01 +0200

    preprocessing

--- classifier.py
+++ classifier.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
 df = pd.read_csv('data/Data_tweets.csv', header=None, names=['polarity', 'id', 'date', 'query', 'user', 'text'])
-print(df.head())
+
+# PREPROCESSING
+def remove_punctuation(text):
+    final = "".join(u for u in text if u not in ("?", ".", ";", ":",  "!",'"', "-"))
+    return final
+df['text'] = df['text'].apply(remove_punctuation)
+df = df[['text','polarity']]
